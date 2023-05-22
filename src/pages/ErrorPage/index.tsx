28b24ce0d3613ea32e39374f4a21d3fa85cbae6b
mazur <mazursasha1990@gmail.com>
2023-05-22 23:35:58 +0300

    Added Error page to the routing

--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -1,8 +1,14 @@
+import { NavLink } from 'react-router-dom';
+import { routerConstants } from '../../shared/constants/routes';
+
+const { HOME } = routerConstants;
+
 const ErrorPage = () => {
   return (
     <div>
       <h1>404 Not Found</h1>
       <h3>Something wen wrong </h3>
+      <NavLink to={HOME.path}>Back to {HOME.name}</NavLink>
     </div>
   );
 };
